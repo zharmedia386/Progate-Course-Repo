8ff7ce23e57e0723b05c947c4509b98c076abcf0
zharmedia386 <muhammadazharalauddin@gmail.com>
2021-09-29 08:00:50 +0700

    Javascript ES6 Complete

---
+++ Javascript ES6/es6_study_5/page3/script.js
@@ -0,0 +1,8 @@
+// Remove the code below
+
+// Remove the code above
+
+// Import the constant dog
+import dog from "./dogData";
+
+dog.info();
---
+++ Javascript ES6/es6_study_6/page2/script.js
@@ -0,0 +1,4 @@
+const characters = ["Ken the Ninja", "Ben the Baby Ninja", "Master Wooly", "Birdie"];
+
+// Print all elements in the characters array by using the forEach method
+characters.forEach((character) => {console.log(character)});
